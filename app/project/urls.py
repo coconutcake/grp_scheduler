c03a2503e526b8b529c2146bf1a8b77325213761
Mateusz I <contact@mign.pl>
2021-07-02 16:17:18 +0200

    major update

--- app/project/urls.py
+++ app/project/urls.py
@@ -7,6 +7,7 @@ from drf_yasg import openapi
 from django.views.generic import RedirectView
 from django.urls import reverse
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -39,4 +40,5 @@ urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
